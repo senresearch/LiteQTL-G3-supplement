f4a49f00fd711cac81b713f788faeabc95aaede6
Chelsea Trotter <chelsea.xhu@gmail.comwq>
2020-08-06 16:34:39 -0500

    added code to get x axis names, in the middle of generating plot

--- code/generate_figure.jl
+++ code/generate_figure.jl
@@ -1,6 +1,12 @@
 
 # generate figure for dgemm 
-using DataFrames
+using DataFrames, CSV
+
+using PyCall
+pygui(:tk)
+using PyPlot
+
+
 
 gemm = CSV.read("../data/gemm-timing/dgemm-result.csv", missingstring="NA", header=0, comment="#")
 header = [:m, :n, :p, :req_mem_gb, :avail_mem_gb, :cpu, :gpu, :speedup]
@@ -13,4 +19,19 @@ gemm_no_missing[gemm_no_missing.req_mem_gb .> 8, :]
 
 # get the ones that have a speedup of over 1.7 
 # gemm_no_missing[gemm_no_missing.speedup .> 1.7, :]
+selected = gemm_no_missing[gemm_no_missing.req_mem_gb .> 9, :]
 
+function num2k_string(number::Int)
+    if number > 1000
+        return string(number)[1:end-3]*"k"
+    else 
+        return string(number)
+    end
+end
+
+mDim = num2k_string.(selected[:m])
+nDim = num2k_string.(selected[:n])
+pDim = num2k_string.(selected[:p])
+
+xname = string..(mDim,", ",nDim,", ",pDim)
+speedup = log2.(collect(selected[:speedup]));
