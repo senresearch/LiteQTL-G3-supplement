adf123929e6643bea10f8bbcc31b78551d486640
Saunak Sen <sen@uthsc.edu>
2020-10-27 14:32:09 -0500

    Accommodate sex column; print number of chrs as check.

--- code/rqtl2scan.R
+++ code/rqtl2scan.R
@@ -2,9 +2,10 @@ scan <- function(rqtlfile, cleanphenofile, genoprobfile, rqtlresultfile, maxlod)
     library(qtl)
     bxd <- read.cross(file=rqtlfile,format="csvr",
                     crosstype="risib",genotypes=c("B","D"))
+    print(paste("Number of chromosomes is", nchr(bxd)))
     print("done read.cross. ")
     #drop obs. & traits with all NAs
-    keepidx<-which(rowSums(is.na(bxd$pheno))<ncol(bxd$pheno))
+    keepidx<-which(rowSums(is.na(bxd$pheno))<ncol(bxd$pheno)-1)
     c1<-subset(bxd,ind=keepidx)
     end<-dim(c1$pheno)[2]
 
@@ -17,6 +18,7 @@ scan <- function(rqtlfile, cleanphenofile, genoprobfile, rqtlresultfile, maxlod)
     library(parallel)
     library(qtl2)
     cvt1<-convert2cross2(c1)
+    print(paste("Number of chromosomes is", n_chr(cvt1)))
     map <- insert_pseudomarkers(cvt1$gmap, step=0)
     prtime <- system.time({
         pr <- calc_genoprob(cvt1, map, error_prob=0.002, cores=1)
@@ -61,10 +63,10 @@ scan(rqtlfile, cleanphenofile, genoprobfile, rqtlresultfile, maxlod)
 
 ## Run scan for hippo data. 
 
-# rqtlfile <- "../data/processed/hippo-geno-pheno-rqtl.csv"
-# cleanphenofile <- "../data/processed/hippo-pheno-nomissing.csv"
-# genoprobfile <- "../data/processed/hippo-bxd-genoprob.csv"
-# rqtlresultfile <- "../data/results/hippo_rqtl_lod_score.csv"
-# maxlod = TRUE 
+rqtlfile <- "../data/processed/hippo-geno-pheno-rqtl.csv"
+cleanphenofile <- "../data/processed/hippo-pheno-nomissing.csv"
+genoprobfile <- "../data/processed/hippo-bxd-genoprob.csv"
+rqtlresultfile <- "../data/results/hippo_rqtl_lod_score.csv"
+maxlod = TRUE 
 
 scan(rqtlfile, cleanphenofile, genoprobfile, rqtlresultfile, maxlod)
